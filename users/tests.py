e279b337a5f0bed1b4cfe517cb4290e1c83917f1
haeminchoi2 <sjhk02@gmail.com>
2022-11-15 09:35:08 +0900

    done

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from users.models import User
 
-# Create your tests here.
+class UserLoginTest(APITestCase):
+    def setUp(self):
+        self.data = {'username':'test', 'password':'testpassword', 'fullname':'required :(', 'email':'test@test.com'}
+    
+    def test_signup(self):
+        url = reverse('user_view')
+        response = self.client.post(url, self.data)
+        self.assertEqual(response.data['message'], '가입 완료!!')
